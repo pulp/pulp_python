fbc42b8b17ea628b33ebf6a01f16775f0d6da057
Randy Barlow <rbarlow@redhat.com>
2014-09-26 14:51:47 -0400

    Added admin extension from pulp_ostree.

---
+++ extensions_admin/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pulp_python_extensions_admin',
+    version='0.1.0',
+    packages=find_packages(),
+    url='http://www.pulpproject.org',
+    license='GPLv2+',
+    author='Pulp Team',
+    author_email='pulp-list@redhat.com',
+    description='pulp-admin extensions for python package support',
+    entry_points={
+        'pulp.extensions.admin': [
+            'repo_admin = pulp_python.extensions.admin.pulp_cli:initialize',
+        ]
+    }
+)
