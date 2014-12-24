d2de4f3052dd23eea8d7ffd7b1c91e3fdbdd18e5
Randy Barlow <rbarlow@redhat.com>
2014-12-24 18:09:59 -0500

    Set the exit code correctly in run-tests.py.

--- run-tests.py
+++ run-tests.py
@@ -3,6 +3,7 @@
 
 import os
 import subprocess
+import sys
 
 from pulp.devel.test_runner import run_tests
 
@@ -11,7 +12,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 subprocess.call(['find', PROJECT_DIR, '-name', '*.pyc', '-delete'])
 
 config_file = os.path.join(PROJECT_DIR, 'flake8.cfg')
-subprocess.call(['flake8', '--config', config_file, PROJECT_DIR])
+flake8_exit_code = subprocess.call(['flake8', '--config', config_file, PROJECT_DIR])
 
 PACKAGES = [PROJECT_DIR, 'pulp_python', ]
 
@@ -25,4 +26,6 @@ PLUGIN_TESTS = ['plugins/test/unit/']
 dir_safe_all_platforms = [os.path.join(os.path.dirname(__file__), x) for x in TESTS]
 dir_safe_non_rhel5 = [os.path.join(os.path.dirname(__file__), x) for x in PLUGIN_TESTS]
 
-run_tests(PACKAGES, dir_safe_all_platforms, dir_safe_non_rhel5)
+tests_exit_code = run_tests(PACKAGES, dir_safe_all_platforms, dir_safe_non_rhel5)
+
+sys.exit(flake8_exit_code or tests_exit_code)
