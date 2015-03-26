dbaf02a5d0490bd7af59b58014c7ad768ecd09b9
Randy Barlow <rbarlow@redhat.com>
2015-03-26 12:14:13 -0400

    Set the version to 1.0.0-0.2.rc.

--- extensions_admin/setup.py
+++ extensions_admin/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pulp_python_extensions_admin',
-    version='1.0.0b1',
+    version='1.0.0c2',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
