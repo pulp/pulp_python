b389872e45142ca613e5b9b1fc38fdf67d4773ea
Dennis Kliban <dkliban@redhat.com>
2015-06-30 10:22:22 -0400

    Bumping build number for 1.0.1 beta

--- plugins/setup.py
+++ plugins/setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pulp_python_plugins',
-    version='1.0.1b0',
+    version='1.0.1b1',
     packages=find_packages(),
     url='http://www.pulpproject.org',
     license='GPLv2+',
