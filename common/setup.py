0862ecf39c7a97fa13f141735b53ed8f4a8d611b
Dennis Kliban <dkliban@redhat.com>
2016-03-04 16:08:02 -0500

    Bumping version to 1.1.0-0.5.beta

--- common/setup.py
+++ common/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pulp_python_common',
-    version='1.1.0b4',
+    version='1.1.0b5',
     packages=find_packages(exclude=['test']),
     url='http://www.pulpproject.org',
     license='GPLv2+',
