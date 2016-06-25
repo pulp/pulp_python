20a1836267b222782a7958e336527f90bf64559a
Austin Macdonald <asmacdo@gmail.com>
2016-06-25 14:14:47 -0400

    Populate `packagetype` which is a required field
    `packagetype` has been added to the model and is a required field. It
    was not necessary when there was a single packagetype, sdist, but there
    are now also wheels. We could probably assume that any package without a
    package type is an sdist, but to be on the safe side we also check that
    the filename has the correct file extension.

--- plugins/pulp_python/plugins/migrations/0002_smaller_model_for_json.py
+++ plugins/pulp_python/plugins/migrations/0002_smaller_model_for_json.py
@@ -1,6 +1,7 @@
 """
 This migration removes `home_page`, `platform`, 'author_email`, `description` and `license` from
-the units_python_package collection. Additionally, it renames `_filename` to `filename`.
+the units_python_package collection, it renames `_filename` to `filename`, and it populates the
+new required field, `packagetype`.
 """
 from pulp.server.db import connection
 
@@ -11,7 +12,18 @@ def migrate(*args, **kwargs):
     """
     db = connection.get_database()
     collection = db['units_python_package']
+    set_packagetype(collection)
+    update_fields(collection)
+    collection.drop_indexes()
 
+
+def update_fields(collection):
+    """
+    Updates field names and removes unecessary fields.
+
+    :param collection: collection to update
+    :type  collection: pymongo.collection.Collection
+    """
     collection.update({}, {"$unset": {"home_page": True}}, multi=True)
     collection.update({}, {"$unset": {"platform": True}}, multi=True)
     collection.update({}, {"$unset": {"license": True}}, multi=True)
@@ -20,3 +32,22 @@ def migrate(*args, **kwargs):
     collection.update({}, {"$unset": {"author_email": True}}, multi=True)
 
     collection.update({}, {"$rename": {"_filename": "filename"}}, multi=True)
+
+
+def set_packagetype(collection):
+    """
+    This sets the new `packagetype` field. This migration should only operate on sdists
+    because they were the only supported type before this migration.
+
+    :param collection: collection to update
+    :type  collection: pymongo.collection.Collection
+    """
+    tarball = {"filename": {"$regex": "tar.gz$"}}
+    bzip = {"filename": {"$regex": "tar.bz2$"}}
+    zipfile = {"filename": {"$regex": "zip$"}}
+
+    sdist_filetype = {"$or": [tarball, bzip, zipfile]}
+    no_packagetype = {"packagetype": {"$exists": 0}}
+    set_sdist = {"$set": {"packagetype": "sdist"}}
+
+    collection.update({"$and": [sdist_filetype, no_packagetype]}, set_sdist, multi=True)
--- plugins/test/unit/plugins/migrations/test_0002_smaller_model_for_json.py
+++ plugins/test/unit/plugins/migrations/test_0002_smaller_model_for_json.py
@@ -11,16 +11,31 @@ PATH_TO_MODULE = 'pulp_python.plugins.migrations.0002_smaller_model_for_json'
 migration = _import_all_the_way(PATH_TO_MODULE)
 
 
+@mock.patch.object(migration.connection, 'get_database')
 class TestMigrate(unittest.TestCase):
     """
     Test the migrate() function.
+    """
+    @mock.patch.object(migration, "set_packagetype")
+    @mock.patch.object(migration, "update_fields")
+    def test_calls(self, mock_set_pkg_type, mock_update_fields, mock_get_database):
+        collection = mock_get_database.return_value['archived_calls']
+        migration.migrate()
+        mock_set_pkg_type.assert_called_once_with(collection)
+        mock_update_fields.assert_called_once_with(collection)
+        collection.drop_indexes.assert_called_once_with()
+
+
+class TestUpdateFields(unittest.TestCase):
+    """
+    Test that unnecessary fields are removed and `_filename` is renamed.
     """
 
     @mock.patch.object(migration.connection, 'get_database')
     def test_repos_collection_id_renamed(self, mock_get_database):
         mock_get_database.return_value.collection_names.return_value = []
         collection = mock_get_database.return_value['archived_calls']
-        migration.migrate()
+        migration.update_fields(collection)
         collection.update.assert_has_calls([
             mock.call({}, {"$unset": {"home_page": True}}, multi=True),
             mock.call({}, {"$unset": {"platform": True}}, multi=True),
@@ -30,3 +45,33 @@ class TestMigrate(unittest.TestCase):
             mock.call({}, {"$unset": {"author_email": True}}, multi=True),
             mock.call({}, {"$rename": {"_filename": "filename"}}, multi=True)
         ])
+
+
+class TestSetPackagetype(unittest.TestCase):
+    """
+    Test that packagetype is set.
+    """
+
+    @mock.patch.object(migration.connection, 'get_database')
+    def test_packages_set_calls(self, mock_get_database):
+        """
+        Ensure update is called with the correct parameters.
+        """
+        mock_get_database.return_value.collection_names.return_value = []
+        collection = mock_get_database.return_value['archived_calls']
+        migration.set_packagetype(collection)
+        search_params = {
+            "$and": [
+                {
+                    "$or": [
+                        {"filename": {"$regex": "tar.gz$"}},
+                        {"filename": {"$regex": "tar.bz2$"}},
+                        {"filename": {"$regex": "zip$"}},
+                    ]
+                },
+                {"packagetype": {"$exists": 0}}
+            ]
+        }
+        set_command = {"$set": {"packagetype": "sdist"}}
+
+        collection.update.assert_called_once_with(search_params, set_command, multi=True)
