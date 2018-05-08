d07cc8507c36c3b8535a1500dfb1dcc04e175c65
werwty <bihan.zh@gmail.com>
2018-05-08 13:35:54 -0400

    Prepare setup.py for release
    Update tests to account for smash changes
    https://github.com/PulpQE/pulp-smash/pull/993

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
     'pulpcore-plugin',
     'pkginfo'
 ]
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
     name='pulp-python',
-    version='3.0.0b1.dev0',
+    version='3.0.0b1',
     description='pulp-python plugin for the Pulp Project',
+    long_description=long_description,
     license='GPLv2+',
     author='Pulp Project Developers',
     author_email='pulp-list@redhat.com',
     url='http://www.pulpproject.org',
     install_requires=requirements,
     include_package_data=True,
-    packages=['pulp_python', 'pulp_python.app'],
+    packages=find_packages(exclude=['tests']),
+    classifiers=(
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: POSIX :: Linux',
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
     entry_points={
         'pulpcore.plugin': [
             'pulp_python = pulp_python:default_app_config',
