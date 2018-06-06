6df4dac053fc3a22f893c892523665f9c40c7299
werwty <bihan.zh@gmail.com>
2018-06-06 16:24:03 -0400

    Bump version to beta3, add releaes notes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst') as f:
 
 setup(
     name='pulp-python',
-    version='3.0.0b2',
+    version='3.0.0b3',
     description='pulp-python plugin for the Pulp Project',
     long_description=long_description,
     license='GPLv2+',
