5fcd81f5795efb9c692a02709c9b4373f77a71a3
Daniel Alley <dalley@redhat.com>
2018-12-03 13:35:29 -0500

    Add test to make sure files are not being left open after sync tasks
    re #4073
    https://pulp.plan.io/issues/4073

--- pulp_python/tests/functional/api/test_sync.py
+++ pulp_python/tests/functional/api/test_sync.py
@@ -1,6 +1,6 @@
 import unittest
-from pulp_smash import api, config, exceptions
-from pulp_smash.pulp3.constants import REPO_PATH
+from pulp_smash import api, cli, config, exceptions
+from pulp_smash.pulp3.constants import MEDIA_PATH, REPO_PATH
 from pulp_smash.pulp3.utils import (
     gen_repo,
     get_added_content,
@@ -84,6 +84,37 @@ class BasicPythonSyncTestCase(unittest.TestCase):
         self.assertNotEqual(latest_version_href, repo['_latest_version_href'])
         self.assertEqual(len(get_content(repo)), PYTHON_XS_PACKAGE_COUNT)
         self.assertEqual(len(get_added_content(repo)), 0)
+
+    def test_file_decriptors(self):
+        """Test whether file descriptors are closed properly.
+
+        This test targets the following issue:
+        `Pulp #4073 <https://pulp.plan.io/issues/4073>`_
+
+        Do the following:
+        1. Check if 'lsof' is installed. If it is not, skip this test.
+        2. Create and sync a repo.
+        3. Run the 'lsof' command to verify that files in the
+           path ``/var/lib/pulp/`` are closed after the sync.
+        4. Assert that issued command returns `0` opened files.
+        """
+        cli_client = cli.Client(self.cfg, cli.echo_handler)
+
+        # check if 'lsof' is available
+        if cli_client.run(('which', 'lsof')).returncode != 0:
+            raise unittest.SkipTest('lsof package is not present')
+
+        repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, repo['_href'])
+
+        remote = self.client.post(PYTHON_REMOTE_PATH, gen_python_remote())
+        self.addCleanup(self.client.delete, remote['_href'])
+
+        sync(self.cfg, remote, repo)
+
+        cmd = 'lsof -t +D {}'.format(MEDIA_PATH).split()
+        response = cli_client.run(cmd).stdout
+        self.assertEqual(len(response), 0, response)
 
 
 class SyncInvalidURLTestCase(unittest.TestCase):
