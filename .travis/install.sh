c980886e46532a880639f229b7aaa7b6ce7a9b5b
Bruno Rocha <rochacbruno@gmail.com>
2019-02-13 18:07:32 -0200

    Update install machinery to resolve specific Pulp-Smash PRs

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,7 @@ set -v
 export COMMIT_MSG=$(git show HEAD^2 -s)
 export PULP_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulp\/pull\/(\d+)' | awk -F'/' '{print $7}')
 export PULP_PLUGIN_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/pulp\/pulpcore-plugin\/pull\/(\d+)' | awk -F'/' '{print $7}')
+export PULP_SMASH_PR_NUMBER=$(echo $COMMIT_MSG | grep -oP 'Required\ PR:\ https\:\/\/github\.com\/PulpQE\/pulp-smash\/pull\/(\d+)' | awk -F'/' '{print $7}')
 
 pip install -r test_requirements.txt
 
@@ -28,6 +29,16 @@ if [ -n "$PULP_PLUGIN_PR_NUMBER" ]; then
 fi
 
 pip install -e ./pulpcore-plugin
+
+if [ -n "$PULP_SMASH_PR_NUMBER" ]; then
+  pip uninstall -y pulp-smash
+  git clone https://github.com/PulpQE/pulp-smash.git
+  pushd pulp-smash
+  git fetch origin +refs/pull/$PULP_SMASH_PR_NUMBER/merge
+  git checkout FETCH_HEAD
+  popd
+  pip install -e ./pulp-smash
+fi
 
 cd pulp_python
 pip install -e .
