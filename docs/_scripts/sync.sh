bf313e344092b033c384d649c26f5ca182c1e973
Eduardo Orochena <mint.alchemist@gmail.com>
2020-06-06 15:23:04 -0400

    fixes #6918 https://pulp.plan.io/issues/6918

--- docs/_scripts/sync.sh
+++ docs/_scripts/sync.sh
@@ -4,8 +4,9 @@ export TASK_URL=$(http POST $BASE_ADDR$REPO_HREF'sync/' remote=$REMOTE_HREF mirr
 
 # Poll the task (here we use a function defined in docs/_scripts/base.sh)
 # When the task is complete, it gives us a new repository version
-wait_for_pulp $TASK_URL
-export REPOVERSION_HREF=${CREATED_RESOURCE[0]}
+wait_until_task_finished $BASE_ADDR$TASK_URL
+echo "Set REPOVERSION_HREF from finished task."
+export REPOVERSION_HREF=$(http $BASE_ADDR$TASK_URL| jq -r '.created_resources | first')
 
 # Lets inspect our newly created RepositoryVersion
 http $BASE_ADDR$REPOVERSION_HREF
