0b08ca50ef0255e3b3c346a8ea8b319a0d754cef
David Davis <daviddavis@redhat.com>
2020-06-09 16:31:47 -0400

    Update docs script to no longer use example package from fedorapeople
    ref #6655

--- docs/_scripts/artifact.sh
+++ docs/_scripts/artifact.sh
@@ -1,5 +1,5 @@
 # Get a Python package or choose your own
-curl -O https://repos.fedorapeople.org/pulp/pulp/fixtures/python-pypi/packages/shelf-reader-0.1.tar.gz
+curl -O https://files.pythonhosted.org/packages/3a/e3/a6954c4134a899c0006515fbd40208922572947e960b35d0d19fd5a1b3d0/shelf-reader-0.1.tar.gz
 export PKG="shelf-reader-0.1.tar.gz"
 
 # Upload it as an Artifact
