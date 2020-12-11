16cb61143a9952c21e23967ff7f05c77e0cc8526
Gerrod Ubben <gerrodubben@gmail.com>
2020-12-11 17:20:31 -0500

    Moves version to PulpPluginAppConfig
    [noissue]

--- pulp_python/__init__.py
+++ pulp_python/__init__.py
@@ -1,3 +1,1 @@
-__version__ = '3.0.0b13.dev'
-
 default_app_config = 'pulp_python.app.PulpPythonPluginAppConfig'
--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -6,5 +6,6 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
     Entry point for pulp_python plugin.
     """
 
-    name = 'pulp_python.app'
-    label = 'python'
+    name = "pulp_python.app"
+    label = "python"
+    version = "3.0.0b13.dev"
