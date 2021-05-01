b89f34a06fea900105c867d80bc272fe434424e0
Bob Fahr <20520336+bfahr@users.noreply.github.com>
2021-05-01 11:59:28 -0500

    Modify syntax for adding repo content in the docs
    * Identifed in pulp python plugin 3.2.0 and cli 0.8.0
    * Fixes #368
    
    Signed-off-by: Bob Fahr <20520336+bfahr@users.noreply.github.com>

--- docs/_scripts/add_content_repo.sh
+++ docs/_scripts/add_content_repo.sh
@@ -1,5 +1,5 @@
 # Add created PythonPackage content to repository
-pulp python repository add --name foo --filename "shelf-reader-0.1.tar.gz"
+pulp python repository content add --repository foo --filename "shelf-reader-0.1.tar.gz"
 
 # After the task is complete, it gives us a new repository version
 pulp python repository version show --repository foo
