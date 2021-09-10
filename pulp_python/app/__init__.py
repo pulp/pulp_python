9c7ec8a528b346d0a924e31c4ebe4d79d85a5be8
pulpbot <pulp-infra@redhat.com>
2021-09-10 19:27:12 +0000

    Bump to 3.5.2.dev
    [noissue]

--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -8,4 +8,4 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.5.1"
+    version = "3.5.2.dev"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as f:
 
 setup(
     name="pulp-python",
-    version="3.5.1",
+    version="3.5.2.dev",
     description="pulp-python plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
