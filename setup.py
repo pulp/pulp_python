3b557628eaae318137444b38716dc43036cf587a
pulpbot <pulp-infra@redhat.com>
2021-12-15 18:30:34 +0000

    Bump to 3.6.1.dev
    [noissue]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md") as f:
 
 setup(
     name="pulp-python",
-    version="3.6.0",
+    version="3.6.1.dev",
     description="pulp-python plugin for the Pulp Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
