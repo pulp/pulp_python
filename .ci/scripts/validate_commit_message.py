c1eba81696dff652397df96f16a24f3d5ad2c46b
pulpbot <pulp-infra@redhat.com>
2022-03-28 09:40:55 +0000

    Update CI files
    [noissue]

--- .ci/scripts/validate_commit_message.py
+++ .ci/scripts/validate_commit_message.py
@@ -8,6 +8,7 @@
 import re
 import subprocess
 import sys
+import warnings
 from pathlib import Path
 
 
@@ -31,7 +32,11 @@ def __check_status(issue):
     gi = repo.get_issue(int(issue))
     if gi.pull_request:
         sys.exit(f"Error: issue #{issue} is a pull request.")
-    if gi.closed_at:
+    if gi.closed_at and "cherry picked from commit" not in message:
+        warnings.warn(
+            "When backporting, use the -x flag to append a line that says "
+            "'(cherry picked from commit ...)' to the original commit message."
+        )
         sys.exit(f"Error: issue #{issue} is closed.")
 
 
@@ -43,6 +48,8 @@ def __check_changelog(issue):
     for match in matches:
         if match.suffix not in CHANGELOG_EXTS:
             sys.exit(f"Invalid extension for changelog entry '{match}'.")
+        if match.suffix == ".feature" and "cherry picked from commit" in message:
+            sys.exit(f"Can not backport '{match}' as it is a feature.")
 
 
 print("Checking commit message for {sha}.".format(sha=sha[0:7]))
