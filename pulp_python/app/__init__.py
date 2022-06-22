56504d9f95194d61d385d0ccc783c8e4ed8b4c4c
pulpbot <pulp-infra@redhat.com>
2022-06-22 19:21:56 +0000

    Release 3.7.0 GH Issues: 462,505,503
    [noissue]

--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -8,5 +8,5 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.7.0.dev"
+    version = "3.7.0"
     python_package_name = "pulp-python"
