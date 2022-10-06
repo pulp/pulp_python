c0886cd1d886174bfd32ee2fb9d4ae46be856ff7
pulpbot <pulp-infra@redhat.com>
2022-10-06 15:05:18 +0000

    Bump to 3.7.4.dev
    [noissue]

--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -8,5 +8,5 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.7.3"
+    version = "3.7.4.dev"
     python_package_name = "pulp-python"
