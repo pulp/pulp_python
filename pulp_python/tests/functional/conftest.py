3f0cc0211d7d1f91842b22e613b656e9d9e86324
Gerrod <gerrodubben@gmail.com>
2024-08-13 08:33:04 -0400

    Merge pull request #717 from gerrod3/normalize-name-pypi
    Fix package name normalization for package pypi json view
    
    (cherry picked from commit fc43b4b253dbec5931d6a230296942e4ba52ec3e)

--- pulp_python/tests/functional/conftest.py
+++ pulp_python/tests/functional/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import uuid
 
 from pulp_smash.pulp3.utils import gen_distribution, gen_repo
 from pulp_python.tests.functional.utils import gen_python_remote
@@ -9,6 +10,7 @@ from pulpcore.client.pulp_python import (
     DistributionsPypiApi,
     PublicationsPypiApi,
     RepositoriesPythonApi,
+    RepositoriesPythonVersionsApi,
     RemotesPythonApi,
 )
 
@@ -27,6 +29,12 @@ def python_bindings_client(cid, bindings_cfg):
 def python_repo_api_client(python_bindings_client):
     """Provides the Python Repository API client object."""
     return RepositoriesPythonApi(python_bindings_client)
+
+
+@pytest.fixture
+def python_repo_version_api_client(python_bindings_client):
+    """Provides the Python Repository Version API client object."""
+    return RepositoriesPythonVersionsApi(python_bindings_client)
 
 
 @pytest.fixture
@@ -54,6 +62,16 @@ def python_publication_api_client(python_bindings_client):
 
 
 # Object Generation Fixtures
+
+@pytest.fixture
+def python_repo_factory(python_repo_api_client, gen_object_with_cleanup):
+    """A factory to generate a Python Repository with auto-cleanup."""
+    def _gen_python_repo(**kwargs):
+        kwargs.setdefault("name", str(uuid.uuid4()))
+        return gen_object_with_cleanup(python_repo_api_client, kwargs)
+
+    return _gen_python_repo
+
 
 @pytest.fixture
 def python_repo(python_repo_api_client, gen_object_with_cleanup):
@@ -92,3 +110,43 @@ def python_remote_factory(python_remote_api_client, gen_object_with_cleanup):
         return gen_object_with_cleanup(python_remote_api_client, body)
 
     yield _gen_python_remote
+
+
+@pytest.fixture
+def python_repo_with_sync(
+    python_repo_api_client, python_repo_factory, python_remote_factory, monitor_task
+):
+    """A factory to generate a Python Repository synced with the passed in Remote."""
+    def _gen_python_repo_sync(remote=None, mirror=False, repository=None, **body):
+        kwargs = {}
+        if pulp_domain := body.get("pulp_domain"):
+            kwargs["pulp_domain"] = pulp_domain
+        remote = remote or python_remote_factory(**kwargs)
+        repo = repository or python_repo_factory(**body)
+        sync_body = {"mirror": mirror, "remote": remote.pulp_href}
+        monitor_task(python_repo_api_client.sync(repo.pulp_href, sync_body).task)
+        return python_repo_api_client.read(repo.pulp_href)
+
+    yield _gen_python_repo_sync
+
+
+@pytest.fixture
+def python_content_summary(python_repo_api_client, python_repo_version_api_client):
+    """Get a summary of the repository version's content."""
+    def _gen_summary(repository_version=None, repository=None, version=None):
+        if repository_version is None:
+            repo_href = get_href(repository)
+            if version:
+                repo_ver_href = f"{repo_href}versions/{version}/"
+            else:
+                repo_ver_href = python_repo_api_client.read(repo_href).latest_version_href
+        else:
+            repo_ver_href = get_href(repository_version)
+        return python_repo_version_api_client.read(repo_ver_href).content_summary
+
+    yield _gen_summary
+
+
+def get_href(item):
+    """Tries to get the href from the given item, whether it is a string or object."""
+    return item if isinstance(item, str) else item.pulp_href
