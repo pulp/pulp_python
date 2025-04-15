b85a3fbcb93c81a4865d1620ad57f519ed5be79a
pulpbot <pulp-infra@redhat.com>
2025-04-15 14:59:16 +0000

    Release 3.11.5

--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -8,5 +8,5 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.11.5.dev"
+    version = "3.11.5"
     python_package_name = "pulp-python"
