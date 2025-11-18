173f541b7a5ff5afdbd6211994a5c454163a55c7
pulpbot <pulp-infra@redhat.com>
2025-11-18 15:23:47 +0000

    Bump version: 3.11.7 → 3.11.8.dev

--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -8,5 +8,5 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.11.7"
+    version = "3.11.8.dev"
     python_package_name = "pulp-python"
