31cd66535a5b8f3e55231eb9a981382419f66ef2
Jitka Obselkova <jobselko@redhat.com>
2025-11-21 23:21:39 +0100

    tmp

--- pulp_python/tests/functional/api/test_pypi_apis.py
+++ pulp_python/tests/functional/api/test_pypi_apis.py
@@ -10,6 +10,7 @@ from pulp_python.tests.functional.constants import (
     PYTHON_MD_PYPI_SUMMARY,
     PYTHON_EGG_FILENAME,
     PYTHON_EGG_SHA256,
+    PYTHON_WHEEL_FILENAME,
     PYTHON_WHEEL_SHA256,
     SHELF_PYTHON_JSON,
 )
@@ -137,8 +138,8 @@ def test_package_upload_simple(
     assert summary.added["python.python"]["count"] == 1
 
 
-@pytest.mark.parallel
 def test_wheel_package_upload_with_metadata(
+    delete_orphans_pre,
     python_content_summary,
     python_empty_repo_distro,
     python_package_dist_directory,
