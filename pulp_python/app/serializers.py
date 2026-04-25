25fbf8c2f1d3c9673eeea0fee73824332507ede7
Daniel Alley <dalley@redhat.com>
2026-04-24 22:54:55 -0400

    Resolve ruff check warnings

--- pulp_python/app/serializers.py
+++ pulp_python/app/serializers.py
@@ -411,7 +411,7 @@ class PythonRemoteSerializer(core_serializers.RemoteSerializer):
                 Requirement(pkg)
             except ValueError as ve:
                 raise serializers.ValidationError(
-                    _("includes specifier {} is invalid. {}".format(pkg, ve))
+                    _("includes specifier {} is invalid. {}").format(pkg, ve)
                 )
         return value
 
@@ -422,7 +422,7 @@ class PythonRemoteSerializer(core_serializers.RemoteSerializer):
                 Requirement(pkg)
             except ValueError as ve:
                 raise serializers.ValidationError(
-                    _("excludes specifier {} is invalid. {}".format(pkg, ve))
+                    _("excludes specifier {} is invalid. {}").format(pkg, ve)
                 )
         return value
 
