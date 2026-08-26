b53b283e2afcf8877dbcb91198ecd906aecd9f34
pulpbot <pulp-infra@redhat.com>
2026-08-26 13:27:01 +0000

    Bump minor version

--- CHANGES/1338.feature
+++
@@ -1,1 +0,0 @@
-Added conditional request support (`Last-Modified` / `If-Modified-Since` and `ETag` / `If-None-Match`) to the Simple API and JSON Metadata API for improved cache efficiency.
--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -12,7 +12,7 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.35.0.dev"
+    version = "3.36.0.dev"
     python_package_name = "pulp-python"
     domain_compatible = True
 
