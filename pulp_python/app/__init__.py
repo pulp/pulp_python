13a6a6bb97a33519d16252a93999886005a1fdfb
pulpbot <pulp-infra@redhat.com>
2026-09-03 13:11:02 +0000

    Bump version: 3.36.0 → 3.36.1.dev

--- pulp_python/app/__init__.py
+++ pulp_python/app/__init__.py
@@ -12,7 +12,7 @@ class PulpPythonPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_python.app"
     label = "python"
-    version = "3.36.0"
+    version = "3.36.1.dev"
     python_package_name = "pulp-python"
     domain_compatible = True
 
